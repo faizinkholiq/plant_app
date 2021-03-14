5e25d674e40dd419c279b357ca9298502a215c0f
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-03-15 01:46:45 +0700

    :sparkles: update ui detail screen and styling the button

--- lib/screens/home/components/recomend_plants.dart
+++ lib/screens/home/components/recomend_plants.dart
@@ -55,7 +55,7 @@ class RecomendsPlants extends StatelessWidget {
                     builder: (context) => DetailsScreen(),  
                   )
                 );
-              },
+              },                                                                               
             ),
           ],
         ),
